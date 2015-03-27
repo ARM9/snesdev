d6a38a2c6ef5f923d7e2e2867922ba81510573a2
ARM9 <toast.dslua@gmail.com>
2015-03-27 16:58:53 +0100

    redo triangle

--- gsu/triangle/gsu/triangle.asm
+++ gsu/triangle/gsu/triangle.asm
@@ -2,18 +2,16 @@
     arch snes.gsu
 
     sram0()
-    max_x:; dw 0
-    min_x:; dw 0
-    max_y:; dw 0
-    min_y:; dw 0
-    max_z:; dw 0
-    min_z:; dw 0
+    max_x:; db 0
+    min_x:; db 0
+    max_y:; db 0
+    min_y:; db 0
 
     align(2)
-vertex_buffer:
-    Tri_struct()
+TriangleBuffer:
 
-_edge_buffer:
+    align(2)
+edge_buffer:
     fill 192*(2)
 
     bank0()
@@ -23,12 +21,14 @@ scope drawTriangle: {
 // args:
     define tri_ptr(r3)  // Tri_struct* r3
 // vars:
-    define x1(r1)
-    define y1(r2)
-    define vt(r4) // temporary vector/whatever storage
-    define v0(r5)
-    define v1(r6)
-    define v2(r7)
+    define rx(r1)
+    define ry(r2)
+    define count(r3)    //
+    define v0(r4)
+    define v1(r5)
+    define v2(r6)
+    define vt(r7)   // temporary vector/whatever storage
+    define imm(r8)
     define tmp(r9)
 // clobbers:
 // tbd
@@ -36,31 +36,65 @@ scope drawTriangle: {
     move r0, {tri_ptr}
 
     iwt {tmp}, #max_x
-    ibt r12, #3
+    ibt r12, #2
     move r13, r15
-loop_vertices:
+loop_vertices: {
+    // v0.x v1.x v2.x 
     to {v0}
     ldb (r0)
     // next vertex
-    add #3
+    add #2
     to {v1}
     ldb (r0)
     // next vertex
-    add #3
+    add #2
     to {v2}
     ldb (r0)
 
-    max({v0}, {v1})
-    //move {vt}, r0
+    // max coord
+    max({v0}, {v1})//; move {vt}, r0
     max(r0, {v2})
-
     stb ({tmp})
     inc {tmp}
+
+    // min coord
+    min({v0}, {v1})
+    min(r0, {v2})
+    stb ({tmp})
+    inc {tmp}
+
     // next coordinate
-    sub #5
+    sub #3
     loop
     nop
+}
 
+    // num scanlines
+    // v0 = max_x
+    // v1 = min_x
+    lm r0, (max_y)
+    to {v0}; lob    // max y
+    to {ry}; hib      // min y
+    // count = max_x - min_x
+    to r12; from {ry}; sub {v0}
+    iwt r13, #x_loop
+y_loop: {
+    // todo: store scan lengths 
+    lm {rx}, (max_x)
+    to {v1}; lob    // max x
+    to {rx}; hib    // min x
+    x_loop: {
+        loop
+        plot
+    }
+    todo   with {rx}; sub r12
+    lm {rx}, (edge_buffer)
+
+    inc {ry}
+    from {v0}; sub {ry}
+    bge y_loop
+    nop
+}
 
     ret
     nop
